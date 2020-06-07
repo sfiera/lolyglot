d0ba70a5b90c9a0d04eae033f5df514622788ea8
Chris Pickel <sfiera@twotaled.com>
2020-06-07 10:40:41 +0900

    Simplify Jinja2

--- lol-jinja2.py
+++ lol-jinja2.py
@@ -2,16 +2,18 @@
 
 import jinja2
 
-print(jinja2.Template("".join([
-    "{% set a = 'lol' %}"
-    "{% set b = ' '.join([a, a, a, a, a, a, a, a, a, a]) %}"
-    "{% set c = ' '.join([b, b, b, b, b, b, b, b, b, b]) %}"
-    "{% set d = ' '.join([c, c, c, c, c, c, c, c, c, c]) %}"
-    "{% set e = ' '.join([d, d, d, d, d, d, d, d, d, d]) %}"
-    "{% set f = ' '.join([e, e, e, e, e, e, e, e, e, e]) %}"
-    "{% set g = ' '.join([f, f, f, f, f, f, f, f, f, f]) %}"
-    "{% set h = ' '.join([g, g, g, g, g, g, g, g, g, g]) %}"
-    "{% set i = ' '.join([h, h, h, h, h, h, h, h, h, h]) %}"
-    "{% set j = ' '.join([i, i, i, i, i, i, i, i, i, i]) %}"
-    "{{ j }}"
-])).render())
+TEMPLATE = """
+{% set a = 'lol' %}
+{% set b = ' '.join([a, a, a, a, a, a, a, a, a, a]) %}
+{% set c = ' '.join([b, b, b, b, b, b, b, b, b, b]) %}
+{% set d = ' '.join([c, c, c, c, c, c, c, c, c, c]) %}
+{% set e = ' '.join([d, d, d, d, d, d, d, d, d, d]) %}
+{% set f = ' '.join([e, e, e, e, e, e, e, e, e, e]) %}
+{% set g = ' '.join([f, f, f, f, f, f, f, f, f, f]) %}
+{% set h = ' '.join([g, g, g, g, g, g, g, g, g, g]) %}
+{% set i = ' '.join([h, h, h, h, h, h, h, h, h, h]) %}
+{% set j = ' '.join([i, i, i, i, i, i, i, i, i, i]) %}
+{{ j }}
+""".strip()
+
+print(jinja2.Template(TEMPLATE).render())
